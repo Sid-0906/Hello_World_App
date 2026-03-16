abb577a8e3e919126e463cb76ce4766da3a4b540
Sid-0906 <sm5094@srmist.edu.in>
2026-03-16 18:18:18 +0530

    Implemented UC2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
+        // UC1
         System.out.println("Hello World");
+
+        // UC2
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "! Welcome to the application.");
+        } else {
+            System.out.println("Please provide your name.");
+        }
+
     }
 }

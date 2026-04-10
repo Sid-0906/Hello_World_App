a31f0d28eb4ddcd486b0bdeb9b243dc042cf29cd
Sid-0906 <sm5094@srmist.edu.in>
2026-04-10 13:16:02 +0530

    UC3-Implemented

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        String name;
 
-        // UC1
-        System.out.println("Hello World");
-
-        // UC2
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "! Welcome to the application.");
+            name = args[0];
         } else {
-            System.out.println("Please provide your name.");
+            name = "World";
         }
 
+        System.out.println("Hello, " + name + "!");
     }
 }
